d30d1c1b0fef8e4074b8ccfd2e7f548938719871
Roman Blanyar <romanb@oss-usa.com>
2017-07-05 02:29:30 +0300

    Post postman

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '595c1e66ab417d3d0049e6f511';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+const id = '595c041d6ff3f214045a0d47';
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('User ID not found');
+  }
+
+  console.log('Iser By Id', user);
+}).catch((e) => console.log(e));
