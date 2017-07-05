a32ef992b7bb37f470f7a4ac6effdb663837df1b
Roman Blanyar <romanb@oss-usa.com>
2017-07-06 00:11:35 +0300

    Post postman

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
   }, (e) => {return res.status(404).send(e)});
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
